02f2573000342ff6358595dd7a6a390323079bd1
ARP <andrea_rexa@outlook.es>
2024-05-22 13:39:48 +0200

    icono

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,6 +7,7 @@
     @vite(['resources/js/app.js'])
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="icon" href="{{ asset('iconoApp16.ico') }}" type="image/x-icon">
     <style>
         body {
             background-image: url('/storage/imagenes/fondo.jpg');
