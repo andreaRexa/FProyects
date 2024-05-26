e39caefd15af0db15bdb50bd77dd7b9eb10b6291
ARP <andrea_rexa@outlook.es>
2024-05-26 16:47:41 +0200

    dd

--- app/Http/Controllers/ProyectoController.php
+++ app/Http/Controllers/ProyectoController.php
@@ -49,7 +49,7 @@ class ProyectoController extends Controller
         
         // Obtener los proyectos filtrados
         $proyectos = $query->get();
-        dd($query);
+        dd($query->toSql());
         $ciclos = Ciclo::distinct()->get(); 
         $cursos = AlumnoCiclo::distinct()->get(); 
         
