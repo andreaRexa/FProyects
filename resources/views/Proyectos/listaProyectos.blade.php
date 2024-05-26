3ee429e290ec31e7aebb975e77deb97b1776fc53
ARP <andrea_rexa@outlook.es>
2024-05-26 14:14:06 +0200

    dd

--- resources/views/Proyectos/listaProyectos.blade.php
+++ resources/views/Proyectos/listaProyectos.blade.php
@@ -5,28 +5,30 @@
 @section('content')
     <h1>Listado de Proyectos</h1>
 
-    <table>
-        <thead>
-            <tr>
-                <th>Foto</th>
-                <th>Nombre</th>
-                <th>Descripción</th>
-                <th>Ciclo</th>
-                <th>Curso</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($proyectos as $proyecto)
+    <div class="table-responsive">
+        <table class="table table-striped table-bordered table-hover"> <!-- Agregamos la clase 'table-bordered' para bordes en la tabla y 'table-hover' para resaltar filas al pasar el cursor -->
+            <thead class="thead-dark">
                 <tr>
-                    <td>
-                        <img src="data:image/jpeg;base64,{{ base64_encode($proyecto->FotoProyecto) }}" alt="{{ $proyecto->nombre }}" style="width: 100px;">
-                    </td>
-                    <td>{{ $proyecto->NombreProyecto }}</td>
-                    <td>{{ $proyecto->Descripcion }}</td>
-                    <td>{{ $proyecto->proyectoAlumno->usuario->alumnoCiclo->ciclo->NombreCiclo }}</td>
-                    <td>{{ $proyecto->proyectoAlumno->usuario->alumnoCiclo->FechaCurso }}</td>
+                    <th scope="col">Foto</th> <!-- Usamos 'scope="col"' para especificar que es un encabezado de columna -->
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Descripción</th>
+                    <th scope="col">Ciclo</th>
+                    <th scope="col">Curso</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach($proyectos as $proyecto)
+                    <tr>
+                        <td>
+                            <img src="data:image/jpeg;base64,{{ base64_encode($proyecto->FotoProyecto) }}" alt="{{ $proyecto->nombre }}" style="max-width: 100px;" class="img-fluid">
+                        </td>
+                        <td>{{ $proyecto->NombreProyecto }}</td>
+                        <td>{{ $proyecto->Descripcion }}</td>
+                        <td>{{ $proyecto->proyectoAlumno->usuario->alumnoCiclo->ciclo->NombreCiclo }}</td>
+                        <td>{{ $proyecto->proyectoAlumno->usuario->alumnoCiclo->FechaCurso }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
