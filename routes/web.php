7377c8dfde2a772215d962bf4006f14ca95ad7c2
ARP <andrea_rexa@outlook.es>
2024-05-26 10:58:10 +0200

    da

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,17 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\CerrarSesionController;
 use App\Http\Controllers\Auth\MiPerfilController;
 Route::get('/', function () {
+    return view('/');
+});
+
+Route::get('FProyects', function () {
     return view('home');
 });
 
-Route::get('/loginForm', [LoginController::class, 'showLoginForm'])->name('loginForm');
+
+Route::get('FProyects/loginForm', [LoginController::class, 'showLoginForm'])->name('loginForm');
 Route::post('/login', [LoginController::class, 'login'])->name('login');
-Route::get('/logout', [CerrarSesionController::class, 'cerrarSession'])->name('logout');
-Route::get('/MiPerfil', [MiPerfilController::class, 'showMiPerfil'])->name('MiPerfil');
+Route::get('FProyects/logout', [CerrarSesionController::class, 'cerrarSession'])->name('logout');
+Route::get('FProyects/MiPerfil', [MiPerfilController::class, 'showMiPerfil'])->name('MiPerfil');
 Route::put('/perfil/{id}', [MiPerfilController::class, 'update'])->name('perfil.update');
 Route::post('perfil/updatefoto', [MiPerfilController::class, 'updatefoto'])->name('perfil.updatefoto');
