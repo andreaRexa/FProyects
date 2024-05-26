47bde66bdfe0ed84d06ac5b9581f4f07341711d1
ARP <andrea_rexa@outlook.es>
2024-05-26 14:05:45 +0200

    ddd

--- resources/views/Proyectos/listaProyectos.blade.php
+++ resources/views/Proyectos/listaProyectos.blade.php
@@ -1,9 +1,36 @@
-@extends('layout')
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listado de Proyectos</title>
+</head>
+<body>
+    <h1>Listado de Proyectos</h1>
 
-@section('title', 'Listado proyectos')
-
-@section('content')
-
-    <listado-proyectos :proyectos="@json($proyectos)"></listado-proyectos>
-    
-@endsection
+    <table>
+        <thead>
+            <tr>
+                <th>Foto</th>
+                <th>Nombre</th>
+                <th>Descripción</th>
+                <th>Ciclo</th>
+                <th>Curso</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($proyectos as $proyecto)
+                <tr>
+                    <td>
+                        <img src="data:image/jpeg;base64,{{ base64_encode($proyecto->foto) }}" alt="{{ $proyecto->nombre }}" style="width: 100px;">
+                    </td>
+                    <td>{{ $proyecto->nombre }}</td>
+                    <td>{{ $proyecto->descripcion }}</td>
+                    <td>{{ $proyecto->proyectoAlumno->usuario->alumnoCiclo->ciclo->nombre }}</td>
+                    <td>{{ $proyecto->proyectoAlumno->usuario->alumnoCiclo->FechaCurso }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+</html>
