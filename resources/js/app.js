55d6a346316983e5ae561522427dbdf60c3e3059
ARP <andrea_rexa@outlook.es>
2024-05-26 13:54:37 +0200

    ñ

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,7 @@
 import { createApp } from 'vue';
 import MenuBar from './components/MenuBar.vue';
 import ListadoProyectos from './components/ListadoProyectos.vue';
-
-// Crear la primera aplicación Vue
 const app = createApp({});
 app.component('menu-bar', MenuBar);
+app.component('listadoProyectos', ListadoProyectos);
 app.mount('#app');
-
-// Crear la segunda aplicación Vue
-const app2 = createApp({});
-app2.component('listadoProyectos', ListadoProyectos);
-app2.mount('#app2');
--- resources/views/Proyectos/listaProyectos.blade.php
+++ resources/views/Proyectos/listaProyectos.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Listado proyectos')
 
 @section('content')
-    <div id="app2">
-        <listado-proyectos :proyectos="@json($proyectos)"></listado-proyectos>
-    <div>
+
+    <listado-proyectos :proyectos="@json($proyectos)"></listado-proyectos>
+    
 @endsection
