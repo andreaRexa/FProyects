a883c93db1f0cd465886b119f134387528279eb7
ARP <andrea_rexa@outlook.es>
2024-05-29 20:02:19 +0200

    Modulo

--- app/Http/Controllers/GestionesController.php
+++ app/Http/Controllers/GestionesController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Ciclo;
 use App\Models\Curso;
+use App\Models\Familia;
 
 class GestionesController extends Controller
 {
