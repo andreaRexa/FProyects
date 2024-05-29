abd01abf73bccb5e9d65f9984a49219219fae641
ARP <andrea_rexa@outlook.es>
2024-05-29 16:04:14 +0200

    Modulos

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -15,6 +15,7 @@
             background-position: center; 
         }
     </style>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body> 
     <div id="app">
@@ -29,7 +30,7 @@
         </div>
     </div>
     
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
