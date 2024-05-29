4f0e38c2f459541a61c7b47d3f2dab95cac05fb0
ARP <andrea_rexa@outlook.es>
2024-05-29 15:10:28 +0200

    Modulos

--- resources/views/Gestiones/GestionModulos.blade.php
+++ resources/views/Gestiones/GestionModulos.blade.php
@@ -10,34 +10,35 @@
                 <div class="card-body">
                     <h5 class="card-title">Ciclos y Cursos</h5>
                     <ul class="list-group">
-                        @foreach($ciclosConCursos as $ciclo)
-                            <li class="list-group-item">
-                                <div class="row">
-                                    <div class="col">
-                                        <strong>ID Ciclo:</strong> {{ $ciclo->IdCiclo }}
-                                    </div>
-                                    <div class="col">
-                                        <strong>Nombre Ciclo:</strong> {{ $ciclo->NombreCiclo }}
-                                    </div>
-                                    <div class="col">
-                                        <strong>Familia:</strong> {{ $ciclo->NombreFamilia }}
-                                    </div>
-                                    <div class="col">
-                                        <strong>Cursos:</strong>
-                                    </div>
+                    @foreach($ciclosConCursos as $ciclo)
+                        <li class="list-group-item">
+                            <div class="row">
+                                <div class="col">
+                                    <strong>ID Ciclo:</strong> {{ $ciclo->IdCiclo }}
                                 </div>
-                                <div class="row">
-                                    <div class="col">
-                                        <select class="form-control" id="selectCursos{{ $ciclo->IdCiclo }}">
-                                            <option value="">Seleccionar un curso...</option>
-                                            @foreach($ciclosConCursos->cursos->curso as $curso)
-                                                <option value="{{ $curso->IdCurso }}">{{ $curso->Curso }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
+                                <div class="col">
+                                    <strong>Nombre Ciclo:</strong> {{ $ciclo->NombreCiclo }}
                                 </div>
-                            </li>
-                        @endforeach
+                                <div class="col">
+                                    <strong>Familia:</strong> {{ $ciclo->NombreFamilia }}
+                                </div>
+                                <div class="col">
+                                    <strong>Cursos:</strong>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <select class="form-control" id="selectCursos{{ $ciclo->IdCiclo }}">
+                                        <option value="">Seleccionar un curso...</option>
+                                        @foreach($ciclo->cursos as $curso)
+                                            <option value="{{ $curso->IdCurso }}">{{ $curso->Curso }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        </li>
+                    @endforeach
+
                     </ul>
                 </div>
             </div>
