5d00e11f255bfd9e2a0bfee90cbf47b9a65a5703
ARP <andrea_rexa@outlook.es>
2024-05-29 15:53:23 +0200

    Modulos

--- resources/views/Gestiones/GestionModulos.blade.php
+++ resources/views/Gestiones/GestionModulos.blade.php
@@ -9,48 +9,39 @@
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Ciclos y Cursos</h5>
-                    <div class="row">
-                        <div class="col">
-                            <strong>ID Ciclo</strong>
-                        </div>
-                        <div class="col">
-                            <strong>Nombre Ciclo</strong>
-                        </div>
-                        <div class="col">
-                            <strong>Familia</strong>
-                        </div>
-                        <div class="col">
-                            <strong>Cursos</strong>
-                        </div>
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>ID Ciclo</th>
+                                    <th>Nombre Ciclo</th>
+                                    <th>Familia</th>
+                                    <th>Cursos</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($ciclos as $ciclo)
+                                    <tr class="curso-item">
+                                        <td>{{ $ciclo->IdCiclo }}</td>
+                                        <td>{{ $ciclo->NombreCiclo }}</td>
+                                        <td>{{ $ciclo->NombreFamilia }}</td>
+                                        <td>
+                                            <select class="form-control" id="selectCursos{{ $ciclo->IdCiclo }}">
+                                                <option value="">Listado de curso</option>
+                                                @foreach($ciclo->cursos as $curso)
+                                                    <option value="{{ $curso->IdCurso }}">{{ $curso->curso->Curso }}</option>
+                                                @endforeach
+                                            </select>      
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
-                    <ul class="list-group">
-                        @foreach($ciclos as $ciclo)
-                            <li class="list-group-item">
-                                <div class="row">
-                                    <div class="col">
-                                        {{ $ciclo->IdCiclo }}
-                                    </div>
-                                    <div class="col">
-                                        {{ $ciclo->NombreCiclo }}
-                                    </div>
-                                    <div class="col">
-                                        {{ $ciclo->NombreFamilia }}
-                                    </div>
-                                    <div class="col">
-                                        <select class="form-control" id="selectCursos{{ $ciclo->IdCiclo }}">
-                                            <option value="">Seleccionar un curso...</option>
-                                            @foreach($ciclo->cursos as $curso)
-                                                <option value="{{ $curso->IdCurso }}">{{ $curso->curso->Curso }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                </div>
-                            </li>
-                        @endforeach
-                    </ul>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 @endsection
