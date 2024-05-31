0472251249956c49b0ffc7860c9aff57cf7bdbbb
ARP <andrea_rexa@outlook.es>
2024-05-31 13:52:47 +0200

    Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,13 @@
 
 @section('content')
     <div class="container mt-4">
+        <h2 class="text-center mb-4" style="color: #333; font-size: 24px; font-weight: bold;">Top proyectos del mes</h2>
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div id="photoCarousel" class="carousel slide" data-ride="carousel">
                     <ul class="carousel-indicators">
                         @foreach($proyectos as $index => $proyecto)
-                            <li data-target="#photoCarousel" data-slide-to="{{ $index }}" class="{{ $index == 0 ? 'active' : '' }}"></li> <!-- Usamos $index como identificador único -->
+                            <li data-target="#photoCarousel" data-slide-to="{{ $index }}" class="{{ $index == 0 ? 'active' : '' }}"></li>
                         @endforeach
                     </ul>
 
@@ -17,6 +18,9 @@
                         @foreach($proyectos as $index => $proyecto)
                             <div class="carousel-item {{ $index == 0 ? 'active' : '' }}">
                                 <img src="data:image/jpeg;base64,{{ base64_encode($proyecto->FotoProyecto) }}" alt="{{ $proyecto->NombreProyecto }}" class="d-block w-100" style="max-width: 500px; max-height: 300px; object-fit: cover; margin: 0 auto;">
+                                <div class="carousel-caption d-none d-md-block"> <!-- Mostrar nombre del proyecto en el carrusel --
+                                    <h5>{{ $proyecto->NombreProyecto }}</h5>
+                                </div>
                             </div>
                         @endforeach
                     </div>
