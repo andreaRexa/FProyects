12bd02174895d5f27fcb3a2945c3665ac93b7c8b
ARP <andrea_rexa@outlook.es>
2024-05-31 13:41:33 +0200

    Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,33 +3,36 @@
 @section('title', 'Principal')
 
 @section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div id="photoCarousel" class="carousel slide" data-ride="carousel">
+                    <ul class="carousel-indicators">
+                        <li data-target="#photoCarousel" data-slide-to="0" class="active"></li>
+                        @foreach($proyectos as $index => $proyecto)
+                            <li data-target="#photoCarousel" data-slide-to="{{ $index + 1 }}" class="{{ $index == 0 ? 'active' : '' }}"></li>
+                        @endforeach
+                    </ul>
 
-    <div id="photoCarousel" class="carousel slide" data-ride="carousel">
-        <ul class="carousel-indicators">
-            <li data-target="#photoCarousel" data-slide-to="0" class="active"></li>
-            @foreach($proyectos as $index => $proyecto)
-                <li data-target="#photoCarousel" data-slide-to="{{ $index + 1 }}" class="{{ $index == 0 ? 'active' : '' }}"></li>
-            @endforeach
-        </ul>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="data:image/jpeg;base64,{{ base64_encode($proyectos[0]->FotoProyecto) }}" alt="{{ $proyectos[0]->NombreProyecto }}" class="d-block w-100" style="max-width: 250px; max-height: 150px; object-fit: cover; margin: 0 auto;">
+                        </div>
+                        @foreach($proyectos as $proyecto)
+                            <div class="carousel-item">
+                                <img src="data:image/jpeg;base64,{{ base64_encode($proyecto->FotoProyecto) }}" alt="{{ $proyecto->NombreProyecto }}" class="d-block w-100" style="max-width: 250px; max-height: 150px; object-fit: cover; margin: 0 auto;">
+                            </div>
+                        @endforeach
+                    </div>
 
-        
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="data:image/jpeg;base64,{{ base64_encode($proyectos[0]->FotoProyecto) }}" alt="{{ $proyectos[0]->NombreProyecto }}" class="d-block w-100" style="width: 250px; height: 150px; object-fit: cover;">
-            </div>
-            @foreach($proyectos as $proyecto)
-                <div class="carousel-item">
-                    <img src="data:image/jpeg;base64,{{ base64_encode($proyecto->FotoProyecto) }}" alt="{{ $proyecto->NombreProyecto }}" class="d-block w-100" style="width: 250px; height: 150px; object-fit: cover;">
+                    <a class="carousel-control-prev" href="#photoCarousel" data-slide="prev">
+                        <span class="carousel-control-prev-icon"></span>
+                    </a>
+                    <a class="carousel-control-next" href="#photoCarousel" data-slide="next">
+                        <span class="carousel-control-next-icon"></span>
+                    </a>
                 </div>
-            @endforeach
+            </div>
         </div>
-
-        <a class="carousel-control-prev" href="#photoCarousel" data-slide="prev">
-            <span class="carousel-control-prev-icon"></span>
-        </a>
-        <a class="carousel-control-next" href="#photoCarousel" data-slide="next">
-            <span class="carousel-control-next-icon"></span>
-        </a>
     </div>
-
 @endsection
