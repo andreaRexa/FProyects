4c1725252bf72deb6a8478bbf64693b222ff2de7
ARP <andrea_rexa@outlook.es>
2024-05-31 14:20:05 +0200

    Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,26 +8,26 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div id="photoCarousel" class="carousel slide" data-ride="carousel">
-                    <ul class="carousel-indicators">
-                        @foreach($proyectos as $index => $proyecto)
-                            <li data-target="#photoCarousel" data-slide-to="{{ $index }}" class="{{ $index == 0 ? 'active' : '' }}"></li>
-                        @endforeach
-                    </ul>
                     <div class="carousel-inner">
                         @foreach($proyectos as $index => $proyecto)
                             <div class="carousel-item {{ $index == 0 ? 'active' : '' }}">
+                                <div class="card">
                                 <a href="{{ route('proyectos.detalle', $proyecto->IdProyecto) }}"> <img src="data:image/jpeg;base64,{{ base64_encode($proyecto->FotoProyecto) }}" alt="{{ $proyecto->NombreProyecto }}" class="d-block w-100" style="max-width: 500px; max-height: 300px; object-fit: cover; margin: 0 auto;"></a>
-                                <div class="carousel-caption d-none d-md-block" style="color: #fff; bottom: 20px; width: 100%; left: 0; right: 0; text-align: center;"> 
-                                    <h5 style="font-size: 24px;">{{ $proyecto->NombreProyecto }}</h5>
+                                    <div class="card-body">
+                                        <h5 class="card-title">{{ $proyecto->NombreProyecto }}</h5>
+                                       
+                                    </div>
                                 </div>
                             </div>
                         @endforeach
                     </div>
-                    <a class="carousel-control-prev" href="#photoCarousel" data-slide="prev">
-                        <span class="carousel-control-prev-icon"></span>
+                    <a class="carousel-control-prev" href="#photoCarousel" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Anterior</span>
                     </a>
-                    <a class="carousel-control-next" href="#photoCarousel" data-slide="next">
-                        <span class="carousel-control-next-icon"></span>
+                    <a class="carousel-control-next" href="#photoCarousel" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Siguiente</span>
                     </a>
                 </div>
                 <div class="card mt-4">
