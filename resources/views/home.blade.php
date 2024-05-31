e089cbd82d07ec43e7aa68e9a46dd99724c6a516
ARP <andrea_rexa@outlook.es>
2024-05-31 13:47:59 +0200

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,24 +3,20 @@
 @section('title', 'Principal')
 
 @section('content')
-    <div class="container">
+    <div class="container mt-4">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div id="photoCarousel" class="carousel slide" data-ride="carousel">
                     <ul class="carousel-indicators">
-                        <li data-target="#photoCarousel" data-slide-to="0" class="active"></li>
                         @foreach($proyectos as $index => $proyecto)
-                            <li data-target="#photoCarousel" data-slide-to="{{ $index + 1 }}" class="{{ $index == 0 ? 'active' : '' }}"></li>
+                            <li data-target="#photoCarousel" data-slide-to="{{ $index }}" class="{{ $index == 0 ? 'active' : '' }}"></li> <!-- Usamos $index como identificador único -->
                         @endforeach
                     </ul>
 
                     <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="data:image/jpeg;base64,{{ base64_encode($proyectos[0]->FotoProyecto) }}" alt="{{ $proyectos[0]->NombreProyecto }}" class="d-block w-100" style="max-width: 250px; max-height: 150px; object-fit: cover; margin: 0 auto;">
-                        </div>
-                        @foreach($proyectos as $proyecto)
-                            <div class="carousel-item">
-                                <img src="data:image/jpeg;base64,{{ base64_encode($proyecto->FotoProyecto) }}" alt="{{ $proyecto->NombreProyecto }}" class="d-block w-100" style="max-width: 250px; max-height: 150px; object-fit: cover; margin: 0 auto;">
+                        @foreach($proyectos as $index => $proyecto)
+                            <div class="carousel-item {{ $index == 0 ? 'active' : '' }}">
+                                <img src="data:image/jpeg;base64,{{ base64_encode($proyecto->FotoProyecto) }}" alt="{{ $proyecto->NombreProyecto }}" class="d-block w-100" style="max-width: 500px; max-height: 300px; object-fit: cover; margin: 0 auto;">
                             </div>
                         @endforeach
                     </div>
