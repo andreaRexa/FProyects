c773c7fe1448f3c0f44bbc75079dccd237e153d2
ARP <andrea_rexa@outlook.es>
2024-05-31 15:49:11 +0200

    RecPass

--- app/Http/Controllers/Auth/RecContraseniaController.php
+++ app/Http/Controllers/Auth/RecContraseniaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User; 
+use App\Http\Controllers\Auth\Str;
 
 class RecContraseniaController extends Controller
 {
