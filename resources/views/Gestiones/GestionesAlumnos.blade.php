d375857260f9d2b7d8f694fc74a75d64c2dcc878
ARP <andrea_rexa@outlook.es>
2024-06-02 15:33:21 +0200

    ma

--- resources/views/Gestiones/GestionesAlumnos.blade.php
+++ resources/views/Gestiones/GestionesAlumnos.blade.php
@@ -11,16 +11,14 @@
                 <div class="card-body">
                     <table class="table">
                         <thead>
-                            <tr>
-                                <th>Foto y Nombre</th>
-                            </tr>
+                           
                         </thead>
                         <tbody>
                             @foreach($alumnos->chunk(5) as $chunk)
                                 <tr>
                                     @foreach($chunk as $alumno)
                                         <td class="text-center">
-                                            <img src="data:image/jpeg;base64,{{ base64_encode($alumno->FotoUsuario) }}" alt="Foto de {{ $alumno->Nombre }}" class="img-fluid rounded" style="width: 350px; height: 350px;">
+                                            <img src="data:image/jpeg;base64,{{ base64_encode($alumno->FotoUsuario) }}" alt="Foto de {{ $alumno->Nombre }}" class="img-fluid rounded" style="width: 150px; height: 150px;">
                                             <div>{{ $alumno->Nombre }} {{ $alumno->Apellido }}</div>
                                         </td>
                                     @endforeach
